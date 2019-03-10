88beb49fd92b61c0d96e73bad508e99ef8bde660
chrismooreproductions <chrismooreproductions@gmail.com>
2019-03-10 18:14:20 +0000

    Updated the title component with additional description

--- src/app/components/presentational/Title/index.js
+++ src/app/components/presentational/Title/index.js
@@ -9,6 +9,7 @@ class Title extends React.Component {
           <p>Thank you for taking the time to look over this code demo</p>
           <p>This is a simple app to demonstrate code splitting and the lazy loading of a component using React lazy, Suspense and Webpack 4.</p>
           <p>To get started please click the 'Show family members component' which will fetch and load the FamilyMembersComponent bundle asynchronously.</p>
+          <p>You can verify the lazy loading of the Family Members Component by opening your devtools `Network` tab before clicking the button. When you click the button a network request is sent to load the FamilyMembersComponent bundle.</p>
         </div>
       </div>
     )
